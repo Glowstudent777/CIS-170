366b9d147c704ee4a6dbef684dec9f9f136ac0a6
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-10-31 19:19:24 -0500

    basework for siblings

--- Module 8/Siblings.py
+++ Module 8/Siblings.py
@@ -1,11 +1,29 @@
 # Author: Glowstudent
-# Program: Template
+# Program: Siblings
 
-name: str
+input: int
+
 
 def main():
-    name = str(input("Enter your name: "))
-    print("Hello, {0}!".format(name))
+    global input
+
+    input = input("Enter number of siblings: ")
+
+    if input.isdigit():
+        if int(input) >= 0:
+            if int(input) == 0:
+                print("You are an only child.")
+                exit(0)
+            else:
+                print("You have", input, "siblings.")
+                exit(0)
+        else:
+            print("Invalid entry for number of siblings! Program is exiting!")
+            exit(0)
+    else:
+        print("Invalid entry for number of siblings! Program is exiting!")
+        exit(0)
+
 
 if __name__ == "__main__":
     main()
