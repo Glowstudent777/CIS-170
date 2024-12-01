fdbf32f9f64446c1ef9be933d028cdeb2a9a128b
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-11-30 19:03:43 -0600

    Finish toolbox.py maybe?

--- Module 11/HackingToolbox.py
+++ Module 11/HackingToolbox.py
@@ -6,64 +6,189 @@ from termcolor import colored
 
 input: int
 
+def clearScreen():
+    print("\033[H\033[J")
+
+def centerText(text: str, width: int, fill: str = " "):
+    if len(text) >= width:
+        return text
+    else:
+        padding = width - len(text)
+        leftPadding = padding // 2
+        rightPadding = padding - leftPadding
+
+        return fill * leftPadding + text + fill * rightPadding
+
 def displayHeader():
     # 62 characters wide
     figlet = Figlet(font="small")
     header = figlet.renderText("Hacking Toolbox")
     print(colored(header, "magenta"))
 
+# print() is used to add a blank line, print("\n") does 2 blank lines for some reason
 def displayMainMenu():
-    # 62 - 11 = 51 characters wide
-    # 51 / 2 = 25 characters wide on the left and 26 right
-    print(colored("=" * 25 + " Main Menu " + "=" * 26, "cyan"))
-    print("\n")
-
-    # 99 Has one less space because it is a two digit number and shifts while the others dont and it looks bad :/
-    # Probably a better way to do this but I am unfamiliar with python
+    print(colored(centerText(" Main Menu ", 62, "="), "cyan"))
+    print()
     print(colored("\t1 ", "light_magenta"), colored(" - Password Cracking", "yellow"))
     print(colored("\t2 ", "light_magenta"), colored(" - Cryptography", "yellow"))
     print(colored("\t99 ", "light_magenta"), colored("- Quit Program", "yellow"))
-    print("\n")
+    print()
 
 def displayPasswordCrackingMenu():
-    print("Password Cracking Menu")
+    print(colored(centerText(" Password Cracking Menu ", 62, "="), "cyan"))
+    print()
+    print(colored("\t1 ", "light_magenta"), colored(" - Generate a wordlist", "yellow"))
+    print(colored("\t99 ", "light_magenta"), colored("- Return to Main Menu", "yellow"))
+    print()
 
 def displayCryptographyMenu():
-    print("Cryptography Menu")
+    print(colored(centerText(" Cryptography Menu ", 62, "="), "cyan"))
+    print()
+    print(colored("\t1 ", "light_magenta"), colored(" - Decode ROT 13", "yellow"))
+    print(colored("\t2 ", "light_magenta"), colored(" - Encode ROT 13", "yellow"))
+    print(colored("\t99 ", "light_magenta"), colored("- Return to Main Menu", "yellow"))
+    print()
 
-def generatePasswordList():
-    print("Password List")
+def generatePasswordList(filename: str, prefix: str):
+    outFile = open(filename, "w")
 
-def encodeROT13():
-    print("ROT13")
+    print()
+    print("Generating password list...")
 
-def decodeROT13():
-    print("ROT13")
+    for i in range(10000):
+        outFile.write(prefix + str(i).zfill(4) + "\n")
+    outFile.close()
+
+    print("Password list generated and saved to", filename)
+    print()
+    print("Press Enter to return to the Main Menu")
+    input()
+    main()
+
+def rot13(msg: str):
+    result = ""
+
+    for char in msg:
+        if char.isalpha():
+            asciiValue = ord(char)
+            asciiValue += 13
+
+            if char.islower():
+                if asciiValue > ord("z"):
+                    asciiValue -= 26
+            else:
+                if asciiValue > ord("Z"):
+                    asciiValue -= 26
+
+            result += chr(asciiValue)
+        else:
+            result += char
+
+    return result
 
 def main():
     selection: int = -1
 
+    print("\033]0;Hacking Toolbox\007")
+    clearScreen()
     displayHeader()
     displayMainMenu()
 
     while (selection < 1 or selection > 2) and selection != 99:
         selection = input("Enter Selection: ")
 
-        if selection.isdigit() == False:
+        if (selection.isdigit() == False) or ((int(selection) < 1 or int(selection) > 2) and int(selection) != 99):
             print(colored("Invalid Selection!", "red"))
             selection = -1
         else:
             selection = int(selection)
     
     if selection == 1:
+        filename: str
+        prefix: str
+
+        selection = -1
+
+        clearScreen()
+        displayHeader()
         displayPasswordCrackingMenu()
+
+        while selection != 1 and selection != 99:
+            selection = input("Enter Selection: ")
+
+            if (selection.isdigit() == False) or (int(selection) != 1 and int(selection) != 99):
+                print(colored("Invalid Selection!", "red"))
+                selection = -1
+            else:
+                selection = int(selection)
+        
+        if selection == 1:
+            print()
+            filename = input("Enter filename to store passwords to: ")
+
+            while filename == "":
+                print(colored("Filename cannot be blank!", "red"))
+                filename = input("Enter filename to store passwords to: ")
+
+            prefix = input("Enter prefix for password: ")
+            generatePasswordList(filename, prefix)
+        elif selection == 99:
+            main()
+
     elif selection == 2:
+
+        selection = -1
+
+        clearScreen()
+        displayHeader()
         displayCryptographyMenu()
+
+        while (selection < 1 or selection > 2) and selection != 99:
+            selection = input("Enter Selection: ")
+
+            if (selection.isdigit() == False) or ((int(selection) < 1 or int(selection) > 2) and int(selection) != 99):
+                print(colored("Invalid Selection!", "red"))
+                selection = -1
+            else:
+                selection = int(selection)
+            
+        if selection == 1:
+            decoded: str
+
+            print()
+            msg = input("Enter message to decode: ")
+
+            while msg == "":
+                print(colored("Message cannot be blank!", "red"))
+                msg = input("Enter message to decode: ")
+
+            decoded = rot13(msg)
+            print("Decoded message is:", decoded)
+            print()
+            print("Press Enter to return to the Main Menu")
+            input()
+            main()
+        elif selection == 2:
+            encoded: str
+
+            print()
+            msg = input("Enter message to encode: ")
+
+            while msg == "":
+                print(colored("Message cannot be blank!", "red"))
+                msg = input("Enter message to encode: ")
+
+            encoded = rot13(msg)
+            print("Encoded message is:", encoded)
+            print()
+            print("Press Enter to return to the Main Menu")
+            input()
+            main()
+        elif selection == 99:
+            main()
     elif selection == 99:
         print(colored("Goodbye!", "red"))
         exit(0)
-
-
 
 if __name__ == "__main__":
     main()
